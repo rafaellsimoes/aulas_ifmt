8aa08fb51f9baad992585d27a1a847b23f27534b
Rafael <48397061+rafaellsimoes@users.noreply.github.com>
2025-06-03 09:11:27 -0400

    Delete programacao_desktop/aula_02_06/Fracao.java

--- programacao_desktop/aula_02_06/Fracao.java
+++
@@ -1,9 +0,0 @@
-class Fracao {
-    public static double quociente(double numerador, double denominador) throws ArithmeticException {
-        if (denominador == 0) {
-            throw new ArithmeticException("Divisão por zero!"); 
-            
-        }
-        return numerador / denominador;
-    }
-}
