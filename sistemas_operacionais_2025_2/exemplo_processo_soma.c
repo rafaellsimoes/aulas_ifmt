8c8066834b19fb280ce985c75f2f4c73c1c33382
Rafael <48397061+rafaellsimoes@users.noreply.github.com>
2025-08-20 09:00:16 -0400

    Create exemplo_processo_soma.c

---
+++ sistemas_operacionais_2025_2/exemplo_processo_soma.c
@@ -0,0 +1,55 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num1, num2;
+    char cmdLine[100];
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    sprintf(cmdLine, "cmd.exe /c echo Processo filho: %d + %d = %d", 
+            num1, num2, num1  + num2);
+    
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+    
+    // Criar processo filho
+    if (!CreateProcessA(
+        NULL,           // Nome do executável
+        cmdLine,        // Linha de comando
+        NULL,           // Segurança do processo
+        NULL,           // Segurança da thread
+        FALSE,          // Não herdar handles
+        0,              // Flags
+        NULL,           // Ambiente
+        NULL,           // Diretório
+        &si,            // Startup info
+        &pi             // Process info
+    )) {
+        printf("Erro ao criar processo: %d\n", GetLastError());
+        return 1;
+    }
+    
+    printf("Processo pai: Criou processo filho com ID %d\n", pi.dwProcessId);
+    
+ 
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    
+    printf("Processo pai: Processo filho terminou\n");
+   
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    printf("Pressione Enter para sair...");
+    getchar(); getchar(); 
+    
+    return 0;
+}
